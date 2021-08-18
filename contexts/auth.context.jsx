5cd9803478fa62f7e426ce422beed1421f564f3b
unimatrix2 <dnny.nis@gmail.com>
2021-08-18 09:47:16 -0300

    added auth context & store

---
+++ contexts/auth.context.jsx
@@ -0,0 +1,29 @@
+import { useReducer, createContext } from "react";
+import userStore from "./stores/user.store";
+
+const initialState = {
+	deviceWindow: {},
+	user: {},
+};
+
+const Context = createContext({});
+
+const combineReducers =
+  (...reducers) =>
+  (state, action) => {
+    for (let i = 0; i < reducers.length; i++) {
+      state = reducers[i](state, action);
+    }
+    return state;
+  };
+
+const Provider = ({ children }) => {
+  const [state, dispatch] = useReducer(
+    combineReducers(userStore),
+    initialState
+  );
+  const value = { state, dispatch };
+  return <Context.Provider value={value}>{children}</Context.Provider>;
+};
+
+export { Context, Provider };
---
+++ contexts/stores/user.store.js
@@ -0,0 +1,8 @@
+const userStore = (state, action) => {
+	switch (action.type) {
+		case 'PROVIDE-USER':
+			return { ...state, user: action.payload };
+		default:
+			return state;
+	}
+} 
