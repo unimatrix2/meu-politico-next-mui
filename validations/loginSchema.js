9e4754af70e922ee9cb197be7c6269df3a5f17d3
unimatrix2 <dnny.nis@gmail.com>
2021-08-20 00:16:35 -0300

    added login schema validator

---
+++ validations/loginSchema.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup';
+
+const loginSchema = yup.object({
+    cpf: yup
+        .string()
+        .trim()
+        .matches(/[0-9]/, 'Utilize apenas números')
+        .min(11, 'Precisa conter 11 caracteres')
+        .max(11, 'Precisa conter 11 caracteres')
+        .required('Campo obrigatório'),
+    password: yup
+        .string()
+        .trim()
+        .required('Campo obrigatório'),
+    remember: yup
+        .boolean()
+});
+
+export default loginSchema;
