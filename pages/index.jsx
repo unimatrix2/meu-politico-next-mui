e08cf82103a0fd4af13fc2c252954b99333d75e1
unimatrix2 <dnny.nis@gmail.com>
2021-08-20 00:17:53 -0300

    removed unused imports

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,3 @@
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
@@ -6,7 +5,6 @@ import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import InputMask from 'react-input-mask';
