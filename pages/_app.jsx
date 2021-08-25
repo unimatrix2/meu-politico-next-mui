7a0f849b2efdedb93a8cbe490e3a922d79e7f7dd
unimatrix2 <dnny.nis@gmail.com>
2021-08-25 15:52:51 -0300

    added theme persistence to injection

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,16 +1,28 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Provider as AuthProvider } from '../contexts/auth.context';
 import { createTheme, ThemeProvider } from '@material-ui/core';
-import appTheme from '../styles/theme';
-
-const theme = createTheme(appTheme);
+import { appTheme, darkTheme } from '../styles/theme';
+import { setDefaultTheme, getDefaultTheme } from '../services/theme.service';
+import { Provider as TCProvider } from '../contexts/theme.context';
 
 function MyApp({ Component, pageProps }) {
+
+  let theme = getDefaultTheme();
+  if (theme && theme == 'dark') {
+    theme = createTheme(darkTheme);
+  } else {
+    theme = createTheme(appTheme);
+    setDefaultTheme('light');
+  }
+
+
   return <AuthProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <TCProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </TCProvider>
   </AuthProvider>
 }
 
