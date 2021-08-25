7302e27891cc32c38b00169d58ca369ced36f041
unimatrix2 <dnny.nis@gmail.com>
2021-08-25 15:45:08 -0300

    fixed axios instance config

--- configs/axios.config.js
+++ configs/axios.config.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
-import { get } from '../utils/sessionStorage.util'
-;
 
 const instance = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-    timeout: 3000,
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 3000,
+	withCredentials: true
 });
 
 export default instance;
