252d9ac888477b079956bd5a7e805cac319acc97
unimatrix2 <dnny.nis@gmail.com>
2021-08-25 13:59:12 -0300

    added dark theme

--- styles/theme.js
+++ styles/theme.js
@@ -1,35 +1,47 @@
 import { lightGreen, grey, teal, indigo, blueGrey, common } from '@material-ui/core/colors';
 
-const appTheme = {
-    typography: {
-        allVariants: {
-            color: grey[900]
-        }
-    },
-    palette: {
-        type: 'light',
-        primary: {
-            light: lightGreen[100],
-            main: lightGreen[500],
-            dark: lightGreen[700],
-            contrastText: common.white,
-            
-        },
-        secondary: {
-            light: indigo[100],
-            main: indigo[500],
-            dark: indigo[700],
-            contrastText: common.white
-        }
+export const appTheme = {
+  typography: {
+    allVariants: {
+      color: grey[900]
     }
+  },
+  palette: {
+		type: 'light',
+    primary: {
+      light: lightGreen[100],
+      main: lightGreen[500],
+      dark: lightGreen[700],
+      contrastText: common.white,
+    },
+    secondary: {
+      light: indigo[100],
+      main: indigo[500],
+      dark: indigo[700],
+      contrastText: common.white
+    }
+  }
 };
 
-const darkTheme = {
-    typography: {
-        allVariants: {
-            color: common.white
-        }
+export const darkTheme = {
+  typography: {
+    allVariants: {
+      color: common.white
     }
-}
-
-export default appTheme;
+  },
+  palette: {
+    type: 'dark',
+    primary: {
+      main: indigo[500],
+      light: indigo[100],
+      dark: indigo[700],
+      contrastText: common.white
+    },
+    secondary: {
+      light: lightGreen[100],
+      main: lightGreen[500],
+      dark: lightGreen[700],
+      contrastText: common.white,
+    }
+  }
+};
