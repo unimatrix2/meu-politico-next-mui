13fcd1ab0f0cce22820a8baae79febaa29961e6f
unimatrix2 <dnny.nis@gmail.com>
2021-08-26 01:45:55 -0300

    fixed theme handling

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,38 +1,24 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { Provider as AuthProvider } from '../contexts/auth.context';
-import { createTheme, ThemeProvider } from '@material-ui/core';
-import { appTheme, darkTheme } from '../styles/theme';
-import { setDefaultTheme, getDefaultTheme } from '../services/theme.service';
-import { Provider as TCProvider } from '../contexts/theme.context';
-import { useState, useEffect } from 'react';
+import { ThemeProvider } from '@material-ui/core';
+import { useState, useEffect, useContext } from 'react';
+import handleThemeSetup from '../handlers/theme.handle';
 
 function MyApp({ Component, pageProps }) {
 
   const [theme, setTheme] = useState();
-
+  const [themeTrigger, setThemeTrigger] = useState();
+  pageProps.themeTrigger = setThemeTrigger;
   useEffect(() => {
-    switch (getDefaultTheme()) {
-      case 'light':
-        setTheme(createTheme(appTheme));
-        break;
-      case 'dark':
-        setTheme(createTheme(darkTheme));
-        break;
-      default:
-        setTheme(createTheme(appTheme));
-        setDefaultTheme('light');
-        break;
-    }
-  }, []);
+      handleThemeSetup(setTheme);
+  }, [themeTrigger]);
 
   return <AuthProvider>
-    <TCProvider>
       {theme ? <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider> : null}
-    </TCProvider>
   </AuthProvider>
 }
 
-export default MyApp
+export default MyApp;
