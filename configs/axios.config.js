80de48e51bd65bf29f0baacab89e6db8bb8e20fc
unimatrix2 <dnny.nis@gmail.com>
2021-08-30 23:46:53 -0300

    removed interceptor

--- configs/axios.config.js
+++ configs/axios.config.js
@@ -3,7 +3,12 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   timeout: 3000,
-	withCredentials: true
+	withCredentials: true,
 });
+
+/* instance.interceptors.request.use(function (response) {
+  console.log(response.headers.get('Set-Cookie'));
+  return response;
+}) */
 
 export default instance;
