07d7679b2dc2433a195154e20128a1c857c041fb
unimatrix2 <dnny.nis@gmail.com>
2021-08-30 23:47:06 -0300

    added reauth hook

---
+++ hooks/useAuth.js
@@ -0,0 +1,22 @@
+import { useEffect, useContext } from "react";
+import instance from "../configs/axios.config";
+import { Context } from "../contexts/auth.context";
+
+export const useAuth = () => {
+	const { state, dispatch } = useContext(Context);
+
+	useEffect(() => {
+		if (!state.user?.cpf) {
+			instance.get('/usuario/acesso')
+				.then(data => {
+					dispatch({
+						type: 'PROVIDE-USER',
+						payload: data.data
+					});
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+	}, [state.user]);
+}
