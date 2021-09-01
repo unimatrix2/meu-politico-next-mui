b7c9b62cadca0e18791eec387a3349d82c61c023
unimatrix2 <dnny.nis@gmail.com>
2021-08-31 23:48:16 -0300

    added logout case

--- contexts/stores/user.store.js
+++ contexts/stores/user.store.js
@@ -2,6 +2,8 @@ const userStore = (state, action) => {
 	switch (action.type) {
 		case 'PROVIDE-USER':
 			return { ...state, user: action.payload };
+		case 'LOGOUT':
+			return { ...state, user: {} }
 		default:
 			return state;
 	}
