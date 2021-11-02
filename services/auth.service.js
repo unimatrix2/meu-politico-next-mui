824bbe17294043d2ffc93c0fd0aa3524e0a501df
unimatrix2 <dnny.nis@gmail.com>
2021-11-02 01:01:56 -0300

    added mappers, refactored signup method

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,30 +1,37 @@
-import instance from "../configs/axios.config";
+import instance from '../configs/axios.config';
+import loginFormMapper from '../mappers/loginForm.mapper';
+import signupFormMapper from '../mappers/signupForm.mapper';
 
 export const login = async (form, helpers, dispatch) => {
-    try {
-        const { data } = await instance.post('/usuario/acesso', form);
-        dispatch({
-            type: 'PROVIDE-USER',
-            payload: data
-        });
-    } catch (error) {
-        if (error.response.data.type) {
-            helpers.setFieldError('cpf', 'CPF ou senha incorretos');
-            helpers.setFieldError('password', 'CPF ou senha incorretos');
-        }
-    }
+	try {
+		const { data } = await instance.post(
+			'/usuario/acesso',
+			loginFormMapper(form)
+		);
+		dispatch({
+			type: 'PROVIDE-USER',
+			payload: data,
+		});
+	} catch (error) {
+		if (error.response.data.type) {
+			helpers.setFieldError('cpf', 'CPF ou senha incorretos');
+			helpers.setFieldError('password', 'CPF ou senha incorretos');
+		}
+	}
 };
 
-export const signup = (form) => {
-    instance.post('/usuario/registro', form)
-    .then((data) => {
-        return data.data;
-    })
-    .catch((err) => {
-        return err;
-    });
+export const signup = async (form, helpers, dispatch) => {
+	try {
+		await instance.post(
+			'/usuario/registro',
+			signupFormMapper(form)
+		);
+		return true;
+	} catch (error) {
+		// error handling here
+	}
 };
 
 export const logout = async () => {
-    await instance.get('/usuario/logout');
-}
+	await instance.get('/usuario/logout');
+};
