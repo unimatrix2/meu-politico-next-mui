e9e82182a6e7532ea99428a89f506f987a30bf3c
unimatrix2 <dnny.nis@gmail.com>
2021-11-03 23:28:20 -0300

    added auth header to config

--- configs/axios.config.js
+++ configs/axios.config.js
@@ -1,9 +1,13 @@
 import axios from 'axios';
+import cookies from './cookies.config';
 
 const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-  timeout: 3000,
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+	timeout: 3000,
 	withCredentials: true,
+  headers: {
+    Authorization: cookies.get('token') || ''
+  }
 });
 
 export default instance;
