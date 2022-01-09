766399ca90d61252540fc91e7fe1d52cef108ff0
unimatrix2 <dnny.nis@gmail.com>
2022-01-09 14:15:51 -0300

    implemented component rendering from enum

--- components/navigation/Drawer.jsx
+++ components/navigation/Drawer.jsx
@@ -31,6 +31,7 @@ function AppDrawer({ mobileOnly, trigger, upperDrawer, lowerDrawer, ...props }) 
 	const { window } = props;
 	const [mobileOpen, setMobileOpen] = useState(false);
 	const [currentMenu, setCurrentMenu] = useState(upperDrawer ? upperDrawer[0].text : null);
+  const [currentComponent, setCurrentComponent] = useState(upperDrawer ? upperDrawer[0].component : null)
 	const { state, dispatch } = useContext(Context);
 	const router = useRouter();
 
@@ -198,6 +199,7 @@ function AppDrawer({ mobileOnly, trigger, upperDrawer, lowerDrawer, ...props }) 
 						key={current.text}
 						onClick={() => {
 							setCurrentMenu(current.text);
+              setCurrentComponent(current.component);
 							setMobileOpen(false);
 						}}
 					>
@@ -216,6 +218,7 @@ function AppDrawer({ mobileOnly, trigger, upperDrawer, lowerDrawer, ...props }) 
 						key={current.text}
 						onClick={() => {
 							setCurrentMenu(current.text);
+              setCurrentComponent(current.component);
 							setMobileOpen(false);
 						}}
 					>
@@ -332,35 +335,7 @@ function AppDrawer({ mobileOnly, trigger, upperDrawer, lowerDrawer, ...props }) 
 				}}
 			>
 				<Toolbar />
-				<Typography paragraph>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-					dolor purus non enim praesent elementum facilisis leo vel. Risus at
-					ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-					quisque non tellus. Convallis convallis tellus id interdum velit
-					laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-					adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-					integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-					eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-					quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-					vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-					lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-					faucibus et molestie ac.
-				</Typography>
-				<Typography paragraph>
-					Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-					ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-					elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-					sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-					mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-					risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-					purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-					tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-					morbi tristique senectus et. Adipiscing elit duis tristique
-					sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-					eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-					posuere sollicitudin aliquam ultrices sagittis orci a.
-				</Typography>
+				{currentComponent}
 			</Box>
 		</Box>
 	);
