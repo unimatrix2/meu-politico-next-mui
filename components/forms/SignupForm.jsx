8cc4a98c7eaf5c36fdd958e414a797dd98d023c1
unimatrix2 <dnny.nis@gmail.com>
2022-01-09 13:03:41 -0300

    fixed cursor type on singup formlink

--- components/forms/SignupForm.jsx
+++ components/forms/SignupForm.jsx
@@ -24,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
 		margin: theme.spacing(3, 0, 2),
 	},
 	formLinks: {
+		cursor: 'pointer',
 		[theme.breakpoints.down(420)]: {
 			color: theme.palette.primary.dark,
 		},
