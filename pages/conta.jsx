3d2030043e740e1f80a9688268635b44c62f0503
Danny Nisenbaum <dnny.nis@gmail.com>
2022-03-25 19:54:07 -0300

    optimized page

--- pages/conta.jsx
+++ pages/conta.jsx
@@ -1,13 +1,9 @@
-import { useContext } from 'react';
-
 import { useAuth } from '../hooks/useAuth';
-import { Context } from '../contexts/auth.context';
-import PrimarySearchAppBar from '../components/navigation/AppBar';
 import ResponsiveDrawer from '../components/navigation/Drawer';
 import { upperDrawerItemsEnum, lowerDrawerItemsEnum } from '../enums/accountDrawer.enum';
 
 export default function Account({ themeTrigger }) {
-	const [state, dispatch] = useContext(Context);
+	
 	useAuth();
 
 	return (
